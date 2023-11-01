67c547d25c76aa91b0a786c573db3e5f0669e001
Erwan Foxtail <84564131+Erwan228@users.noreply.github.com>
2023-11-01 10:23:34 +0100

    dfg

---
+++ js/view.js
@@ -0,0 +1,7 @@
+initialView();
+function initialView() {
+    document.getElementById('void').innerhtml = /*html*/ `
+    <button  style="height: 75px; width: 100px; font-size: large;">Filter</button>
+    <h1>Choose a random FFXIV job!</h1>
+    `
+};
